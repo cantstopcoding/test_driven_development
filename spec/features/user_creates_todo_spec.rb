35ca06537e9e945c0cd102c38ea98add86fbb844
cantstopcoding <jonathan_koss@yahoo.com>
2022-02-04 20:24:26 -0500

    create user_creates_todo_spec.rb

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User created todo' do
+  scenario 'successfully' do
+    visit root_path
+
+    click_on 'Add a new todo'
+    fill_in 'Title', with: 'Buy Milk'
+    click_on('Submit')
+
+    expect(page).to have_css '.todos li', text: 'Buy milk'
+  end
+end
